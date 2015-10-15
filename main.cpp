b9174f40845832a149b2f4bd456eed23ce6dbc54
dannys <dasandiro@gmail.com>
2015-10-14 22:45:28 -0600

    SDL2 and CMake

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+int main(int, char**){
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    SDL_Quit();
+    return 0;
+}
